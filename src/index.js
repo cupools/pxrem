67861804650eca6b6e3abd8d834a57411de56dad
liyh <lyh106415@alibaba-inc.com>
2016-10-18 21:15:03 +0800

    update core & test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import postcss from 'postcss'
+import mkdirp from 'mkdirp'
+import path from 'path'
 import fs from 'fs'
 
 const defaultOption = {
@@ -10,8 +12,55 @@ const defaultOption = {
 }
 
 function pxrem(content, opt) {
-  let option = Object.assign({}, opt, defaultOption)
-  return content
+  let option = Object.assign({}, defaultOption, opt)
+  let root = postcss.parse(content)
+  let handle = transform(option)
+
+  root.walkDecls(handle)
+
+  let ret = root.toString()
+  let { output } = option
+
+  if (output) {
+    writeFile(output, ret)
+  }
+
+  return ret
+}
+
+function writeFile(output, content) {
+  let dirname = path.dirname(output)
+  mkdirp.sync(dirname)
+  fs.writeFileSync(output, content)
+}
+
+function transform(opt) {
+  let { root, filter, unitPrecision, keepPix } = opt
+  let isFunction = filter && filter.constructor === Function
+  let isRegExp = filter && filter.constructor === RegExp
+
+  return function (decl) {
+    let { prop, value } = decl
+
+    // ignore situation
+    if (isFunction && !filter(prop, value, decl)) {
+      return
+    } else if (isRegExp && filter.test(prop)) {
+      return
+    } else if (value.slice(-2) !== 'px') {
+      return
+    }
+
+    let count = Number(value.slice(0, -2))
+    let revise = Number((count / root).toFixed(unitPrecision))
+    let expected = { value: revise + 'rem' }
+
+    if (keepPix) {
+      decl.cloneAfter(expected)
+    } else {
+      decl.replaceWith(decl.clone(expected))
+    }
+  }
 }
 
 function wrap(opt) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,74 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-env mocha */
 
+import fs from 'fs'
+
 import './common'
 import pxrem from '../src/index'
 
 describe('index', function () {
   it('should work', function () {
-    pxrem('.a {width: 100px}').should.have.selector('.a')
-      .and.decl('width', '100px')
+    let option = {
+      root: 75,
+      filter: null,
+      unitPrecision: 6,
+      keepPix: false,
+      output: null
+    }
+    pxrem('.a {width: 750px}', option).should.have.selector('.a')
+      .and.decl('width', '10rem')
+  })
+
+  it('should work with root', function () {
+    let option = {
+      root: 1000
+    }
+    pxrem('.a {width: 750px}', option).should.have.selector('.a')
+      .and.decl('width', '0.75rem')
+  })
+
+  it('should work with filter function', function () {
+    let option = {
+      filter: decl => String.includes(decl, 'width')
+    }
+    pxrem('.a {width: 750px; height: 100px;}', option).should.have.selector('.a')
+      .and.decl('width', '10rem')
+      .and.decl('height', '100px')
+  })
+
+  it('should work with filter regexp', function () {
+    let option = {
+      filter: /height/
+    }
+    pxrem('.a {width: 750px; height: 100px;}', option).should.have.selector('.a')
+      .and.decl('width', '10rem')
+      .and.decl('height', '100px')
+  })
+
+  it('should work with unitPrecision', function () {
+    let option = {
+      unitPrecision: 2
+    }
+    pxrem('.a {width: 100px;}', option).should.have.selector('.a')
+      .and.decl('width', '1.33rem')
+  })
+
+  it('should work with keepPix', function () {
+    let option = {
+      keepPix: true
+    }
+    pxrem('.a {width: 750px;}', option).should.have.selector('.a')
+      .and.decl('width', '10rem')
+      // .and.decl('width', '100rem')
+  })
+
+  it('should work with output', function () {
+    let output = 'test/tmp/style.css'
+    let option = { output }
+    pxrem('.a {width: 750px;}', option).should.have.selector('.a')
+      .and.decl('width', '10rem')
+
+    fs.readFileSync(output).should.have.selector('.a')
+      .and.decl('width', '10rem')
   })
 })
